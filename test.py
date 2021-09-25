9875ba890623b5599a8e52c986c05250852bcc43
wofeishenling <wofeishenling@hotmail.com>
2021-09-25 10:50:31 +0800

    。

--- test.py
+++ test.py
@@ -1,12 +1,5 @@
-# 导入urlopen函数
-from urllib.request import urlopen
-# 导入BeautifulSoup
-from bs4 import BeautifulSoup as bf
-# 请求获取HTML
-html = urlopen("http://www.baidu.com/")
-# 用BeautifulSoup解析html
-obj = bf(html.read(),'html.parser')
-# 从标签head、title里提取标题
-title = obj.head.title
-# 打印标题
-print(title)
+# -*- coding:utf-8 -*-
+import uuid
+ 
+print(uuid.uuid1())
+#bf1dfacf-67d8-11e8-9a23-408d5c985711
