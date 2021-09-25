ff6812442073a40d6c9a606985e6762c047007d2
wofeishenling <wofeishenling@hotmail.com>
2021-09-25 10:50:21 +0800

    。

--- 爬虫/第一章/daka.py
+++ 爬虫/第一章/daka.py
@@ -1,27 +1,31 @@
 import requests
+import uuid
 from requests.sessions import session
 
 url = "https://nco.zjgsu.edu.cn/login"
 
 
 my_headers1 = {
+    # 模拟移动端环境
     "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Mobile Safari/537.36"
 }
 
 dat = {
-    "name":"21020100024",
-    "psswd":"034818",
+    "name":"21020100024", #student id
+    "psswd":"034818",# the last six digits of id
 }
-session = requests.session()
+session = requests.session() #用session自动记录上下午cookie
 
 resp = session.post(url,headers = my_headers1,data = dat)
 print(resp.text)
 print("-"*50)
 
 url2 = "https://nco.zjgsu.edu.cn/"
+my_uuid = uuid.uuid1() #随机生成一个uuid
+
 dat2 = {
-    "uuid": "1db683d9-0d2c-4f83-9341-c9478711151c",
-    "locationInfo": "浙江省杭州市江干区",
+    "uuid": my_uuid,
+    "locationInfo": "浙江省杭州市江干区", #所在地
     "currentResd": "浙江省杭州市浙江工商大学下沙校区",
     "fromHbToZjDate": "",
     "fromHbToZj": "C",
